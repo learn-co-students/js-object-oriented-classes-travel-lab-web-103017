fe97f98f3b51b8339a11aa06e935f92bff684a11
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-12 16:38:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year){
+  return (year - this.startDate.getFullYear()-1)
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  avenueToInteger(avenue){
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled(){
+    let horizontal = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontal) + Math.abs(vertical))
+  }
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
